3e3890b9e46129ef11c695af13172de99a2ab34e
Jigar Patel <jigar@JP-machine.local>
2022-08-08 14:56:53 -0400

    Finish home page

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from '../assets/logo/logo.png'
+
+
+const Hero = () => (
+  <div className="text-center hero p-5">
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="100" />
+    <h1 className="mb-4">Transcribe</h1>
+    <p className="lead">
+      A blazing fast ⚡️⚡️ voice to text conversion application.
+      Upload your audio and we'll take care of the rest.
+    </p>
+  </div>
+);
+
+export default Hero;
