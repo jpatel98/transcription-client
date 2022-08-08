03316c39b452ca76dc9344f7308dd5ad08cfea19
Jigar Patel <jigar@JP-machine.local>
2022-08-08 18:02:12 -0400

    Fix navbar style

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,33 +3,33 @@ import React from "react";
 
 const MainNav = () => (
   <div className="navbar-nav ml-5 mt-2">
-     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li className="nav-item active">
+     <ul className="navbar-nav mr-auto mt-2 mt-lg-0 nav-pills ">
+      <li className="nav-item text-dark">
       <NavLink
         to="/"
         exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
+        className="nav-link px-3"
+        activeClassName="nav-link active"
       >
         Home
       </NavLink>
     </li>
-    <li className="nav-item active">
+    <li className="nav-item text-dark">
       <NavLink
         to="/profile"
         exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
+        className="nav-link px-3"
+        activeClassName="nav-link active"
       >
         Profile
       </NavLink>
     </li>
-    <li className="nav-item active">
+    <li className="nav-item text-dark">
       <NavLink
         to="/transcribe"
         exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
+        className="nav-link px-3"
+        activeClassName="nav-link active"
       >
         Transcribe
       </NavLink>
