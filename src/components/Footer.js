4f171e8071900aa0c9eeea87e2208d7e6afd4652
Jigar Patel <jigar@JP-machine.local>
2022-08-09 10:50:46 -0400

    Add footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light text-center">
+    <p className="m-4">
+      Made by Jigar Patel
+    </p>
+  </footer>
+);
+
+export default Footer;
